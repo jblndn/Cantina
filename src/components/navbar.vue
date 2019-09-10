193e54c33f6c6d901b172b80488c4fd62dddfb10
jb.lindner <jb.lindner@orange.fr>
2019-09-10 18:32:38 +0200

    init project

---
+++ src/components/navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <nav class="container-menu">
+            <ul class="menu">
+                <li class="item-menu">
+                    <a href="#" class="font-menu">Accueil</a>
+                </li>
+                <li class="item-menu">
+                    <a href="#" class="font-menu">Ajouter une recette</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'navbar',
+}
+</script>
+
+
---
+++ src/pages/home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container centered">
+        
+        <navbar></navbar>
+        <h1 class="title-home">
+            Bienvenue sur Cantina
+        </h1>
+        <div>
+            <card>
+
+            </card>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import card from '../components/card.vue';
+import navbar from '../components/navbar.vue';
+
+export default {
+    name: 'home',
+    components: { 
+        card,
+        navbar
+
+    } 
+};
+</script>
+
